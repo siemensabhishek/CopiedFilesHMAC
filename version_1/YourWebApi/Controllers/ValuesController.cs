aa92abca94b731824a58f9eb9c75fd0f706cb387
Singh <abhishek.singh@siemens-healthineers.com>
2023-06-26 11:13:59 +0530

    removed .git folder from all repo

---
+++ version_1/YourWebApi/Controllers/ValuesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using softaware.Authentication.Hmac;
+
+namespace YourWebApi.Controllers
+{
+    [Authorize(AuthenticationSchemes = HmacAuthenticationDefaults.AuthenticationScheme)]
+    [Route("api")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+
+        [HttpGet("hello")]
+        public IActionResult Get()
+        {
+            return Ok("Hello, World!");
+        }
+    }
+
+}
---
+++ version_2/WPFClientApplication/ViewModel/LoginViewModel.cs
@@ -0,0 +1,214 @@
+﻿using softaware.Authentication.Hmac.Client;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WPFClientApplication.ViewModel
+{
+    public class LoginViewModel : ViewModelBase
+    {
+        // fields
+        private int _username;
+        private int _password;
+        private string _errorMessage;
+        private bool _isViewVisible = true;
+
+
+        public int Username
+        {
+            get { return _username; }
+            set { _username = value; OnPropertyChanged("Username"); }
+        }
+        public int Password
+        {
+            get { return _password; }
+            set { _password = value; OnPropertyChanged("Password"); }
+        }
+        public string ErrorMessage
+        {
+            get { return _errorMessage; }
+            set
+            {
+                _errorMessage = value;
+                OnPropertyChanged(nameof(ErrorMessage));
+            }
+        }
+        public bool IsViewVisible
+        {
+            get { return _isViewVisible; }
+            set
+            {
+                _isViewVisible = value;
+                OnPropertyChanged(nameof(IsViewVisible));
+            }
+        }
+
+
+        private string firstName;
+
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+
+
+        private string lastName;
+
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+
+
+
+        // Commands
+        public ICommand LoginCommand { get; set; }
+        public ICommand RecoverPasswordCommand { get; }
+
+        public ICommand ShowPasswordCommand { get; }
+
+        public ICommand RememberPasswordCommand { get; }
+
+        public LoginViewModel()
+        {
+            LoginCommand = new ViewModelCommand(ExecuteLoginCommand, CanExecuteLoginCommand);
+            RecoverPasswordCommand = new ViewModelCommand(p => ExecuteRecoverPassword("", ""));
+
+        }
+
+        private void ExecuteRecoverPassword(string username, string email)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+
+
+        public void ExecuteLoginCommand(object obj)
+        {
+            var temp = IsValidCustomer(Username, Password);
+            if (!temp.Result)
+            {
+                ErrorMessage = "InValid UserName or Password";
+            }
+            else
+            {
+                MainWindow.token = _AccessToken;
+                MainWindow.refreshToken = _RefreshToken;
+                MainWindow.UserId = Username;
+                MainWindow.ViewIndex = 1;
+            }
+        }
+
+
+
+
+
+        string _AccessToken = "";
+        string _RefreshToken = "";
+
+
+        /*
+        async Task<bool> IsValidCustomer(int CustId, int password)
+        {
+            var url = $"https://localhost:7172/customer/validCustomer/{CustId}/{password}";
+            //  var url = $"https://localhost:7172/customer/helloCustomers";
+            using (var client = new HttpClient())
+            {
+                var msg = new HttpRequestMessage(HttpMethod.Get, url);
+                msg.Headers.Add("User-Agent", "C# Program");
+                var res = client.SendAsync(msg).Result;
+
+                var content = await res.Content.ReadAsStringAsync();
+                var stringContent = Convert.ToString(content);
+                var contentResponse = JsonConvert.DeserializeObject<List<string>>(stringContent);
+                var firstResponse = contentResponse[0];
+                var secondResponse = contentResponse[1];
+                var AccessToken = Convert.ToString(firstResponse);
+                var RefreshToken = Convert.ToString(secondResponse);
+                var refreshtoken1 = contentResponse[1];
+
+                _AccessToken = AccessToken;
+                _RefreshToken = RefreshToken;
+
+                //    string _tokenPayload = GetName(_AccessToken);
+                //  string _tokenPayload2 = _tokenPayload;
+                //Console.WriteLine("The token validation parameters are" + _tokenPayload);
+                return !string.IsNullOrEmpty(stringContent);
+            }
+
+        }
+        */
+
+
+
+        async Task<bool> IsValidCustomer(int CustId, int password)
+        {
+            try
+            {
+                Console.WriteLine("Calling the backend API");
+                //  string apiBaseAddress = "https://localhost:44391/api/hello";
+                string apiBaseAddress = $"https://localhost:7172/customer/validCustomer/{CustId}/{password}";
+                string appId = "4d53bce03ec34c0a911182d4c228ee6c";
+                string apiKey = "A93reRTUJHsCuQSHR+L3GxqOJyDmQpCgps102ciuabc=";
+                var customDelegateHandler = new ApiKeyDelegatingHandler(appId, apiKey)
+                {
+                    InnerHandler = new HttpClientHandler()
+                };
+                //  ApiKeyDelegatingHandler client = new ApiKeyDelegatingHandler();
+                HttpClient _client = new HttpClient(customDelegateHandler);
+                //HttpClient _client = new HttpClient(new ApiKeyDelegatingHandler(appId, apiKey));
+                //   HttpClient _client = new HttpClient();
+
+                HttpResponseMessage response = await _client.GetAsync(apiBaseAddress);
+                Console.WriteLine("sdkfghldsfg");
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseString = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine(responseString);
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine("Failed to call the api end point");
+                    return false;
+                }
+                // Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+
+        }
+
+
+
+
+
+
+
+
+
+        private bool CanExecuteLoginCommand(object obj)
+        {
+            bool canExecute;
+
+            if (Username != 0 && Password != 0)
+            {
+                canExecute = true;
+            }
+
+            else
+            {
+                canExecute = false;
+            }
+            return canExecute;
+        }
+    }
+}
